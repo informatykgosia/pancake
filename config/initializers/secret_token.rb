f44d7c4df5bd2f7e09ab420a0011aef8b7847152
Anna Migas <09_migas@wierzba.wzks.uj.edu.pl>
2012-01-10 18:31:12 +0100

    dodanie .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projekt::Application.config.secret_token = '703001e2d14f6b41e2edca540720234502e5d6cef4d4fdfc335c9c84d5322a7406039d3050e867ec238fc37195b69700ada2fa2cf53fb83c2d28ccafb001d15e'
