a809cd7abc4f90aa74eb6ea700c4031310c49eca
Anna Migas <09_migas@wierzba.wzks.uj.edu.pl>
2012-01-23 19:53:00 +0100

    weryfikacja minimalna w userze

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+attr_accessible (:imie, :nazwisko, :e_mail, :adres)
+
+email_check = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+validates (:imie, :presence => true,
+          :length => { :minimum => 3 })
+validates (:nazwisko, :presence => true,
+          :length => { :minimum => 3 })
+validates (:e_mail, :presence => true,
+                     :format => { :with => email_check },
+                      :uniqueness => { :case_sensitive => false } )
+
 end
---
+++ db/migrate/20120123182610_add_e_mail_uniqueness_index.rb
@@ -0,0 +1,7 @@
+class AddEMailUniquenessIndex < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
