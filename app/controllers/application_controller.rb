461dbb6f148e55882a2e80cca3dd86cae69047a1
Anna Migas <09_migas@wierzba.wzks.uj.edu.pl>
2012-01-24 19:25:50 +0100

    skonczony pdf z autoryzacja

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+
+  private
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+  def current_user
+    return @current_user if definde?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
+  def authenticate
+    if !current_user
+      redirect_to new_user_session_path
+    end
+  end
+  
+  def admin_required
+    if !current_user
+      redirect_to new_user_session_path
+    elsif !current_user.admin?
+      flash[:notice] = "nie masz dostepu"
+      redirect_to root_url
+    end
+  end
 end
